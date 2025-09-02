e91dfc09d0e0f6229b4dc970679ac83db6ac6678
sahilart410 <agahuseynovsahil@gmail.com>
2025-09-03 01:37:53 +0400

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,4 +1,8 @@
-module.exports=async(req,res)=>{
-  res.setHeader('Access-Control-Allow-Origin','*');
-  res.status(200).json({merchant:process.env.MERCHANT_ADDRESS||'6MvDDgZWAvZQH5oHcbzYo4DVk1eiNgi4NzRhc4xBg6hW',usdtMint:process.env.USDT_MINT||'Es9vMFrzaCERZ8XhHk7A9G1x1VQ9v6wY5xZk1Z5hK5eR',network:process.env.SOLANA_NETWORK||'mainnet'});
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json({
+    merchant: process.env.MERCHANT_ADDRESS || '6MvDDgZWAvZQH5oHcbzYo4DVk1eiNgi4NzRhc4xBg6hW',
+    usdtMint: process.env.USDT_MINT || 'Es9vMFrzaCERZ8XhHk7A9G1x1VQ9v6wY5xZk1Z5hK5eR',
+    network: process.env.SOLANA_NETWORK || 'mainnet'
+  });
 };
